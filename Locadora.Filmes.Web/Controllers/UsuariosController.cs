55089ee65cafd7916948521b7b564af92331a47e
Caio Rodrigues <caiorodrigues1@unipam.edu.br>
2020-11-19 22:26:09 -0300

    Modificacoes

---
+++ Locadora.Filmes.Web/Controllers/UsuariosController.cs
@@ -0,0 +1,65 @@
+﻿using Locadora.Filmes.Dados.Entity.Context;
+using Locadora.Filmes.Web.Identity;
+using Locadora.Filmes.Web.ViewModels.Usuarios;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Locadora.Filmes.Web.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        public ActionResult CriarUsuario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CriarUsuario(UsuarioViewModel viewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                var userStore = new UserStore<IdentityUser>(new FilmeIdentityDbContext());
+                var userManager = new UserManager<IdentityUser>(userStore);
+                var identityUser = new IdentityUser
+                {
+                    UserName = viewModel.Email,
+                    Email = viewModel.Email
+                };
+                IdentityResult resultado = userManager.Create(identityUser, viewModel.Senha);
+                if (resultado.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("erro_identity", resultado.Errors.First());
+                    return View(viewModel);
+                }
+            }
+            return View(viewModel);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UsuarioViewModel viewModel)
+        {
+            if(ModelState.IsValid)
+            {
+
+            }
+            return View(viewModel);
+        }
+    }
+}
